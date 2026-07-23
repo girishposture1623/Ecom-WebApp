d57626010038331c5c4edc6d40a971f42aeefcac
girishposture1623 <girishposture1623@gmail>
2026-07-23 14:40:59 +0530

    update

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
