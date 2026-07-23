1cf35b0e9ef3984ebeb84b67df2f78f7a1bad97b
girishposture1623 <girishposture1623@gmail>
2026-07-23 12:43:22 +0530

    update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,57 +7,66 @@ dotenv.config();
 
 // token generate
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-}
-// console.log(generateToken)
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 const registerUser = async (req, res) => {
-    const { name, email, password } = req.body
-    try {
-        const existingUser = await User.findOne({ email })
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exist' })
-        }
-        // HashPassword
-        const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(password, salt)
+  const { name, email, password } = req.body;
 
-        const user = await User.create({ name, email, password: hashPassword })
-        if (user) {
-            const otp = Math.floor(100000 + Math.random() * 900000).toString();
+  try {
+    const existingUser = await User.findOne({ email });
 
-            const message = `Welcome to Shoplix, ${name}!
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    const otp = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const message = `Welcome to Shoplix, ${name}!
 
 Your OTP for registration is: ${otp}`;
 
-            await sendEmail({
-                to: user.email,
-                subject: "Welcome to Shoplix - Your OTP for Registration",
-                text: message,
-            });
+    console.log("Sending Email...");
 
-            res.status(201).json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-                token: generateToken(user._id),
-            });
-        } else {
-            res.status(400).json({
-                message: "Invalid user data",
-            });
-        }
-        // await newUser.save();
-        // res.status(200).json({message: 'User Registerd successfully'})
-    } catch (error) {
-        console.error(error);
+    await sendEmail({
+      to: user.email,
+      subject: "Welcome to Shoplix - Your OTP for Registration",
+      text: message,
+    });
 
-        res.status(500).json({
-            message: error.message,
-        });
-    }
-}
+    console.log("Email Sent Successfully");
+
+    return res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id),
+    });
+
+  } catch (error) {
+    console.error("REGISTER ERROR:", error);
+
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 // Login  User
 
