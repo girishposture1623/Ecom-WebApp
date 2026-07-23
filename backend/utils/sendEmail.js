bf888a97908deadda53bd0035218ff09bd9d9b39
girishposture1623 <girishposture1623@gmail>
2026-07-23 14:08:11 +0530

    update

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,41 +7,18 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-// Verify SMTP connection
-transporter.verify((error) => {
-  if (error) {
-    console.error("❌ SMTP Verify Error:", error);
+transporter.verify((err) => {
+  if (err) {
+    console.error("VERIFY ERROR:", err);
   } else {
-    console.log("✅ Brevo SMTP Connected");
+    console.log("SMTP READY");
   }
 });
 
-const sendEmail = async ({ to, subject, text, html }) => {
-  try {
-    const mailOptions = {
-      from: `"Shoplix" <girishposture253@gmail.com>`, // Verified Sender Email
-      to,
-      subject,
-      text,
-      html,
-    };
-
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log("✅ Email Sent:", info.messageId);
-
-    return info;
-  } catch (error) {
-    console.error("❌ Error Sending Email:", error);
-    throw error;
-  }
-};
-
-export default sendEmail;
+export default transporter;
